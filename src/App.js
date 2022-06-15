810fb1755f0f41d80c9cf99074bfb9ab535ef7f5
Diogo Cesar <d_cesarrodrigues@hotmail.com>
2022-06-15 16:20:50 -0300

    customHook para fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React from "react";
+import useFetch from "./useFetch";
+
 
 const App = () => {
+  const {request, data, loading} = useFetch();
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const {response, json} = await request('https://ranekapi.origamid.dev/json/api/produto')
+    }
+    fetchData();
+  }, [])
+
+  if(loading === true) return <p>Loading...</p>
+  if (data)
   return (
-    <>
-      <div>Hello</div>
-    </>
+      <section>
+        {data.map((produto) => (
+          <div key={produto.id}><h2>{produto.nome}</h2></div>
+        ))}
+      </section>
   );
+  else return null
 };
 export default App;
